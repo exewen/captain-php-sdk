15f83f9787184fa3dfaeebff66ef503e194744b1
exeweb <exeweb@163.com>
2025-03-24 11:59:26 +0800

    commit_message

--- tests/CaptainTest.php
+++ tests/CaptainTest.php
@@ -11,19 +11,16 @@ class CaptainTest extends Base
 
     public function testOrderDetail()
     {
-        LoggerFacade::info('1');
-
-
-//        CaptainFacade::setAccessToken(getenv('CAPTAIN_ACCESS_TOKEN'));
-//        CaptainFacade::setOpenChannelId(getenv('CAPTAIN_OPEN_CHANNEL_ID'));
-//        $params   = [
-//            'page'=> 1,
-//            'rows'=> 2,
-//            'start_modified_time' => time()-3*24*3600,
-//            'end_modified_time' => time(),
-//        ];
-//        $response = CaptainFacade::getShipments($params);
-        $this->assertNotEmpty(1);
+        CaptainFacade::setAccessToken(getenv('CAPTAIN_ACCESS_TOKEN'));
+        CaptainFacade::setOpenChannelId(getenv('CAPTAIN_OPEN_CHANNEL_ID'));
+        $params   = [
+            'page'=> 1,
+            'rows'=> 2,
+            'start_modified_time' => time()-3*24*3600,
+            'end_modified_time' => time(),
+        ];
+        $response = CaptainFacade::getShipments($params);
+        $this->assertNotEmpty($response);
     }
 
 }
